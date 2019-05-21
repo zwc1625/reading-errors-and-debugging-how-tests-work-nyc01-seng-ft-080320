daef6e888ef8f2c10b181699fa09359b118c1a5e
maxwellbenton <maxwellbenton@gmail.com>
2019-05-21 17:24:01 -0400

    adds solution

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+first_number = 4
+second_number = 4
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
