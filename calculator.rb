5b71beff948d091a503a6b5c92c65e4976a7c922
Weichang Zheng <weichang1625@gmail.com>
2020-07-15 20:41:56 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+first_number = 5
+second_number = 10
+sum = first_number + second_number
+difference = first_number - second_number
+product =first_number * second_number
+quotient =first_number / second_number
